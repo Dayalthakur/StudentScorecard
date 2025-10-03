967b4fa1d8fcecfa504598557b97db03db37122d
Dayal Thakur <thakurdaya2001@gmail.com>
2025-10-03 20:32:22 +0530

    Add project files.

---
+++ StudentScorecard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentScorecard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
